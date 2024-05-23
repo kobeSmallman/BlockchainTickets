74ccdf47f4868defd15e3d5de046ed79aa607ecd
Kobe Smallman <kobe4smallman@gmail.com>
2024-05-22 20:49:37 -0600

    Backend and middleware test setup for firebase

---
+++ BlockchainTicketsAPI/Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BlockchainTicketsAPI.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
